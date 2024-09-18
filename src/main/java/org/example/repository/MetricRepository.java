0f62a93e03c0599c6d73c4ea84e78fad415c2c06
krayush29 <kumar.ayush_ftc@external.swiggy.in>
2024-09-18 16:03:44 +0530

    Remove unused imports, Update Repository.

--- src/main/java/org/example/repository/MetricRepository.java
+++ src/main/java/org/example/repository/MetricRepository.java
@@ -16,6 +16,7 @@ public class MetricRepository {
         conversionFactors.put(new ImmutablePair<>(VolumeUnits.LITER,VolumeUnits.MILLILITER), 1000.0);
         conversionFactors.put(new ImmutablePair<>(VolumeUnits.LITER, VolumeUnits.CENTILITER), 100.0);
         conversionFactors.put(new ImmutablePair<>(VolumeUnits.LITER, VolumeUnits.DECILITER), 10.0);
+        conversionFactors.put(new ImmutablePair<>(VolumeUnits.LITER,VolumeUnits.LITER), 1.0);
         conversionFactors.put(new ImmutablePair<>(VolumeUnits.LITER, VolumeUnits.DECALITER), 0.1);
         conversionFactors.put(new ImmutablePair<>(VolumeUnits.LITER, VolumeUnits.KILOLITER), 0.001);
 
@@ -29,6 +30,7 @@ public class MetricRepository {
         conversionFactors.put(new ImmutablePair<>(LengthUnits.METER, LengthUnits.MILLIMETER), 1000.0);
         conversionFactors.put(new ImmutablePair<>(LengthUnits.METER, LengthUnits.CENTIMETER), 100.0);
         conversionFactors.put(new ImmutablePair<>(LengthUnits.METER, LengthUnits.DECIMETER), 10.0);
+        conversionFactors.put(new ImmutablePair<>(LengthUnits.METER, LengthUnits.METER), 1.0);
         conversionFactors.put(new ImmutablePair<>(LengthUnits.METER, LengthUnits.DECAMETER), 0.1);
         conversionFactors.put(new ImmutablePair<>(LengthUnits.METER, LengthUnits.KILOMETER), 0.001);
 
@@ -42,6 +44,7 @@ public class MetricRepository {
         conversionFactors.put(new ImmutablePair<>(WeightUnits.GRAM, WeightUnits.MILLIGRAM), 1000.0);
         conversionFactors.put(new ImmutablePair<>(WeightUnits.GRAM, WeightUnits.CENTIGRAM), 100.0);
         conversionFactors.put(new ImmutablePair<>(WeightUnits.GRAM, WeightUnits.DECIGRAM), 10.0);
+        conversionFactors.put(new ImmutablePair<>(WeightUnits.GRAM, WeightUnits.GRAM), 1.0);
         conversionFactors.put(new ImmutablePair<>(WeightUnits.GRAM, WeightUnits.DECAGRAM), 0.1);
         conversionFactors.put(new ImmutablePair<>(WeightUnits.GRAM, WeightUnits.KILOGRAM), 0.001);
 
--- src/test/java/org/example/service/LengthTest.java
+++ src/test/java/org/example/service/LengthTest.java
@@ -1,6 +1,7 @@
 package org.example.service;
 
 import org.example.enums.LengthUnits;
+import org.example.enums.VolumeUnits;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
